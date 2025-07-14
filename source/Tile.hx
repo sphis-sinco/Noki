1b4cd78bf0151b11255376a8d3c8e69b30e547cc
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-14 16:32:02 -0500

    add tile class

---
+++ source/Tile.hx
@@ -0,0 +1,41 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
+
+class Tile extends FlxSprite
+{
+	override public function new(tileType:TileType, position:FlxPoint)
+	{
+		super(position.x, position.y);
+
+		loadGraphic('assets/tiles.png', true, 8, 8);
+		animation.add('tileset', [0, 1, 2, 3, 4], 0, false);
+
+		switch (tileType)
+		{
+			case TILE:
+				animation.frameIndex = 0;
+			case FALLING_TILE:
+				animation.frameIndex = 1;
+			case PLAYER:
+				animation.frameIndex = 2;
+			case KEY:
+				animation.frameIndex = 3;
+			case DOOR:
+				animation.frameIndex = 4;
+		}
+	}
+}
+
+enum TileType
+{
+	// Palette 1
+	TILE;
+	FALLING_TILE;
+
+	// Palette 2
+	PLAYER;
+	KEY;
+	DOOR;
+}
